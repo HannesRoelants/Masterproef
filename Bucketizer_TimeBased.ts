a718074d587cd90c3b038279551624df8a53fd5e
HannesRoelants <hannes.roelants@ugent.be>
2023-05-25 19:20:14 +0200

    Non chronological Bucket added - bugs adding relations fixed - Still does not find a view (bug)

--- Bucketizer_TimeBased.ts
+++ Bucketizer_TimeBased.ts
@@ -4,11 +4,12 @@ import {BucketizerCore} from '@treecg/bucketizer-core';
 import {BucketizerCoreOptions, RelationParameters} from '@treecg/types';
 import {RelationType, LDES} from '@treecg/types';
 
-export interface timeBasedBucket{
+export interface timeBasedBucket {
     startTime: Date;
     endTime: Date;
     pageNumber: number;
 }
+
 export class TimeBucketizerFactory implements Factory<BucketizerCoreOptions> {
     type: string = "time";
 
@@ -16,6 +17,7 @@ export class TimeBucketizerFactory implements Factory<BucketizerCoreOptions> {
         const total: TimeInputType = Object.assign(config, {lastTimestamp: new Date().getTime()});
         return TimeBucketizer.build(total, state);
     }
+
     ldConfig(quads: RDF.Quad[], subject: RDF.Term): BucketizerCoreOptions | void {
         const out = parseBucketizerCoreOptions(quads, subject);
         if (out.type.value === LDES.custom(this.type)) {
@@ -41,7 +43,7 @@ export class TimeBucketizer extends BucketizerCore<TimeInputType> {
     private constructor(bucketizerOptions: TimeInputType, state?: any) {
         super(bucketizerOptions);
         this.pageNumber = 0;
-        this.lastTimestamp = new Date(Date.parse("2022-04-07T08:08:48Z"));
+        this.lastTimestamp = new Date(Date.parse("2022-08-07T08:00:00Z"));
         this.period = bucketizerOptions.period || (5 * 60 * 1000); // Default period of 5 minutes
         this.minTimestamp = bucketizerOptions.minTimestamp;
         this.existingBuckets = []
@@ -75,35 +77,33 @@ export class TimeBucketizer extends BucketizerCore<TimeInputType> {
         out.push(...this.createSDSRecord(this.factory.namedNode(memberId), [`${this.pageNumber}`]));
         return out;
     };
-    public bucketizeNonChronological(quads: RDF.Quad[], memberId: string, minTimestamp: Date): RDF.Quad[]{
+
+    public bucketizeNonChronological(quads: RDF.Quad[], memberId: string, minTimestamp: Date): RDF.Quad[] {
         const out: RDF.Quad[] = [];
         const quadTimestamp = this.getQuadTimestamp(quads);
         if (quadTimestamp) {
             const quadTime = quadTimestamp.getTime();
-            const lastTime = this.lastTimestamp.getTime();
+            const lastTime = (this.lastTimestamp.getTime());
             const timeDiff = quadTime - lastTime;
+            const bucketsToCreate = Math.ceil(timeDiff / this.period);
             if (timeDiff > 0) {
-                const bucketsToCreate = Math.floor(timeDiff / this.period);
-                for (let i = 0; i < bucketsToCreate + 1; i++) {
+                for (let i = 0; i <= (bucketsToCreate + 1); i++) {
                     const bucketStart = new Date(minTimestamp.getTime() + (this.bucketCounter * this.period));
                     const bucketEnd = new Date(minTimestamp.getTime() + (this.bucketCounter * this.period) + this.period);
-                    this.increasePageNumber();
                     const currentPage = this.pageNumber;
-                    const parameters = this.createRelationParameters(currentPage, bucketStart, this.factory.namedNode('http://www.w3.org/ns/sosa/resultTime'));
+                    this.increasePageNumber();
+                    const parameters = this.createRelationParameters(this.pageNumber, bucketStart, this.factory.namedNode('http://www.w3.org/ns/sosa/resultTime'));
                     this.setHypermediaControls(`${currentPage}`, parameters);
                     out.push(...this.expandRelation(`${currentPage}`, parameters));
                     const newBucket: timeBasedBucket = {
-                        startTime: bucketStart,
-                        endTime: bucketEnd,
-                        pageNumber: currentPage
+                        startTime: bucketStart, endTime: bucketEnd, pageNumber: currentPage
                     };
                     this.existingBuckets.push(newBucket);
                     this.increaseBucketNumber();
-                    }
-                console.log((minTimestamp.getTime() + (this.bucketCounter * this.period)) + "BucketStart")
-                console.log(quadTimestamp + "added member")
-                out.push(...this.createSDSRecord(this.factory.namedNode(memberId), [`${this.pageNumber}`]));
-                this.setLastTimestamp(new Date(minTimestamp.getTime() * this.bucketCounter * this.period + bucketsToCreate * this.period));
+                }
+                out.push(...this.createSDSRecord(this.factory.namedNode(memberId), [`${(this.pageNumber - 1)}`]));
+                const newTimeStamp = new Date(minTimestamp.getTime() + (this.bucketCounter * this.period) + this.period);
+                this.setLastTimestamp(newTimeStamp);
             } else {
                 const existingBucketIndex = this.findExistingBucketIndex(quadTimestamp);
                 if (existingBucketIndex !== -1) {
@@ -111,12 +111,11 @@ export class TimeBucketizer extends BucketizerCore<TimeInputType> {
                     const addPage = existingBucket.pageNumber;
                     out.push(...this.createSDSRecord(this.factory.namedNode(memberId), [`${addPage}`]));
                 }
-                console.log((this.existingBuckets[existingBucketIndex].startTime) + "BucketStart")
-                console.log(quadTimestamp + "added member")
             }
         }
         return out;
     }
+
     private findExistingBucketIndex(quadTimestamp: Date): number {
         for (let i = 0; i < this.existingBuckets.length; i++) {
             const bucket = this.existingBuckets[i];
@@ -126,6 +125,7 @@ export class TimeBucketizer extends BucketizerCore<TimeInputType> {
         }
         return -1;
     }
+
     public exportState = (): any => {
         const state = super.exportState();
         state.lastTimestamp = this.lastTimestamp;
